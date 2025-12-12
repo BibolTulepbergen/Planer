691ac95193004e32138900b476591f5b2dc42322
Бибол <moldahasank@gmail.com>
2025-12-13 01:29:01 +0500

    Обновление конфигурации окружения разработки в package.json и vite.config.ts
    - Изменено значение переменной окружения в скрипте dev на CLOUDFLARE_ENV.
    - Упрощена конфигурация Vite, убрано определение режима разработки через переменную WRANGLER_ENV.

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,13 @@ import react from '@vitejs/plugin-react'
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const isDev = mode === 'development' || process.env.WRANGLER_ENV === 'dev';
-  
+export default defineConfig(() => {
   return {
     plugins: [
       react(), 
       cloudflare({
         configPath: './wrangler.jsonc',
         persistState: true,
-        ...(isDev && { environment: 'dev' }),
       })
     ],
     server: {
