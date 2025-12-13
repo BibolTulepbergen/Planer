6dd181e7702d36a7a63583355528e8cf202653bc
Бибол <moldahasank@gmail.com>
2025-12-13 14:23:59 +0500

    Обновление конфигурации Capacitor и скриптов сборки в package.json
    - Закомментирована секция server в capacitor.config.ts для упрощения конфигурации.
    - Добавлен новый скрипт build:mobile в package.json для сборки мобильного приложения с использованием полного домена API.
    - Обновлен скрипт cap:android для использования нового скрипта сборки мобильного приложения.
    - Изменен способ определения базового URL API в api.ts для поддержки различных окружений.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,15 @@ const config: CapacitorConfig = {
   appId: 'com.planer.app',
   appName: 'planer',
   webDir: 'dist/client',
-  server: {
-    url: 'https://planer.moldahasank.workers.dev',
-    androidScheme: 'https'
+  // server: {
+  //   url: 'https://planer.moldahasank.workers.dev',
+  //   androidScheme: 'https'
+  // },
+plugins: {
+  "edge-to-edge": {
+    "backgroundColor": "#ffffff"
   }
+}
 };
 
 export default config;
